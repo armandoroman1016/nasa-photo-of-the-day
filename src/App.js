4d60232c2b03ba722a1e2c470c93262f34fcd84b
Armando Roman <armandoroman1016@gmail.com>
2019-07-17 21:26:00 -0700

    calender centered...styling still needed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Header from './components/header-components/header'
 import TextContent from "./components/text-components/text-content";
 import Media from './components/media-components/media'
 import Footer from './components/footer-components/footer'
-import Calendar from 'react-calendar'
-
+import CreateCalendar from './components/calendar-component/calendar'
 
 function App() {
   
@@ -56,48 +55,8 @@ function App() {
   return (
     <div className="App">
       <Header date={date}/>
-      <Calendar onClickDay = {(event) => { 
-        //split string of event date to allow myself to manipulate data
-        const splitStr = event.toString().split(' ')
-        let tempDate = []
-        tempDate.push(splitStr[3],splitStr[1],splitStr[2])
-        // pushed split string items into order that i need dates
-        const monthIndex ={
-          Jan : '01',
-          Feb : '02',
-          Mar : '03',
-          Apr : '04',
-          May : '05',
-          Jun : '06',
-          Jul : '07',
-          Aug : '08',
-          Sep : '09',
-          Oct : '10',
-          Nov : '11',
-          Dec : '12'
-        }
-        //create a final date variable 
-
-        let extractedDate = ''
-
-        //checking monthIndex to find the matching month
-        //then replacing that months value in the tempDate array
-
-        for (let key in monthIndex){
-          if (key === tempDate[1]){
-            let monthReplacement = monthIndex[`${[tempDate[1]]}`]
-            tempDate.splice(1, 1, monthReplacement)
-            let finalDate = tempDate.join('-')
-            extractedDate += finalDate
-            
-          }
-        }
-
-        //setting api date
-        setApiDate(extractedDate)
-        
-      }}/>
       <TextContent title={title} text={text}/>
+      <CreateCalendar setApiDate={setApiDate} apiDate={apiDate}/>
       <Media mediaUrl={mediaUrl} mediaType={mediaType}/>
       <Footer footerText={footerText}/>
     </div>
